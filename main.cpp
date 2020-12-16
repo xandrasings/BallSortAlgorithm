2a29817ad5a80abbee500e49602c327ed4e2b48d
Xandra <xandrabest@gmail.com>
2020-12-16 14:30:10 -0600

    hard code first two levels

--- main.cpp
+++ main.cpp
@@ -4,45 +4,17 @@
 
 int main() {
 
-	// std::vector < std::vector < Color > > position {
-	// 	{RED, RED, RED},
-	// 	{BLUE, BLUE, BLUE},
-	// 	{RED, BLUE}
-	// };
-
-	// std::vector < std::vector < Color > > position {
-	// 	{},
-	// 	{ORANGE},
-	// 	{BLUE, BLUE, BLUE, BLUE},
-	// 	{RED, RED, RED, RED},
-	// 	{PINK},
-	// 	{GREEN, GREEN, LIME, GREEN},
-	// 	{GRAY, GRAY},
-	// 	{CYAN, CYAN, CYAN, CYAN},
-	// 	{GREEN, GREEN, GREEN, GREEN},
-	// 	{PURPLE, PURPLE, PURPLE, PURPLE},
-	// 	{FOREST, FOREST, FOREST, FOREST},
-	// 	{BROWN, BROWN, BROWN, BROWN},
-	// 	{YELLOW, YELLOW, YELLOW, YELLOW},
-	// 	{}
-	// };
-
+	// LEVEL 1
 	std::vector < std::vector < Color > > position {
-		{},
-		{ORANGE, ORANGE, ORANGE, ORANGE},
-		{BLUE, BLUE, BLUE, BLUE},
-		{RED, RED, RED, RED},
-		{PINK, PINK, PINK, PINK},
-		{GREEN, GREEN, GREEN, GREEN},
-		{GRAY, GRAY, GRAY, GRAY},
-		{CYAN, CYAN, CYAN, CYAN},
-		{GREEN, GREEN, GREEN, GREEN},
-		{PURPLE, PURPLE, PURPLE, PURPLE},
-		{FOREST, FOREST, FOREST, FOREST},
-		{BROWN, BROWN, BROWN, BROWN},
-		{YELLOW, YELLOW, YELLOW, YELLOW},
-		{}
+		{ORANGE, ORANGE, ORANGE},
+		{ORANGE}
 	};
+
+	// // LEVEL 2
+	// std::vector < std::vector < Color > > position {
+	// 	{BLUE, ORANGE, BLUE, ORANGE},
+	// 	{ORANGE, BLUE, ORANGE, BLUE}
+	// };
 
 	Move startPosition(position);
 	startPosition.print();
